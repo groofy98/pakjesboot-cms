270ac0f6a0b2930f08a640f43015384c897cb558
Sjors Grooff <634293@student.inholland.nl>
2021-10-26 10:06:10 +0200

    correct deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:groofy98/pakjesboot-cms.git main:gh-pages
+git push -f git@github.com:groofy98/pakjesboot-cms.git master:gh-pages
 
 cd -
