f7f7a9b2d29ae0f552abcb9cd0953771343df8a2
Sjors Grooff <634293@student.inholland.nl>
2021-10-26 10:00:27 +0200

    shiit

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
-}
+  ]  
+},
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/pakjesboot-cms/'
-    : '/'
+      ? '/pakjesboot-cms/'
+      : '/'
 }
